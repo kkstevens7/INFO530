5b17b23b875353caa135062a3f3b529884f81511
kkstevens7 <kkstevens@vcu.edu>
2025-04-24 18:21:31 -0400

    Updated App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from './LoginPage';
+import StudentDashboard from './StudentDashboard_index';
+
+function App () {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/dashboard" element={<StudentDashboard />} />
+    </Routes>
+  </BrowserRouter>
+  );
+}
+
+export default App;
